5cc893b939547cf66c5096d4b96b0c443adac162
Onlyveira <davileandro46@gmail.com>
2023-05-04 16:40:55 -0300

    Iniciando o projeto

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+            System.out.print("Alô mundo!");
+            System.out.println("Concluí a aula 01 e agora estou mergulhando em java");
+        }
+    }
